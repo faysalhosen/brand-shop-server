664637c50ffa031b94e16830a36013ff077c5030
faysalhosen <faysanhosen7025@gmail.com>
2023-10-22 12:44:31 +0600

    added brand data post method

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
- const express = require('express');
- const cors = require('cors');
- require("dotenv").config()
- const { MongoClient, ServerApiVersion } = require('mongodb');
- const app = express()
- const port = process.env.PORT || 5000;
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
 
- //middleware
- app.use(cors());
- app.use(express.json());
+//middleware
+app.use(cors());
+app.use(express.json());
 
- //brandShop
- //hLR4Fn9UAkdZoJcl
+//brandShop
+//hLR4Fn9UAkdZoJcl
 
- console.log(process.env.DB_USER)
- console.log(process.env.DB_PASS)
-
- 
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASS);
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wzxk65v.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
+console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,33 +24,161 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     await client.connect();
-    const brandNameCollection = client.db('brandDB').collection('brandName');
+    const brandNameCollection = client.db("brandDB").collection("brandName");
+
+    const appleCollection = client.db("brandDB").collection("apple");
+    const samsungCollection = client.db("brandDB").collection("samsung");
+    const asusCollection = client.db("brandDB").collection("asus");
+    const xiaomiCollection = client.db("brandDB").collection("xiaomi");
+    const sonyCollection = client.db("brandDB").collection("sony");
+    const realmeCollection = client.db("brandDB").collection("realme");
 
 
 
 
-    app.get('/brand', async (req, res) => {
-      const cursor = brandNameCollection.find();
+    
+    app.post("/apple", async (req, res) => {
+      const apple = req.body;
+      const result = await appleCollection.insertOne(apple);
+      res.send(result);
+    });
+
+    app.get('/apple', async (req, res) => {
+      const cursor = appleCollection.find();
       const result = await cursor.toArray();
       res.send(result);
   });
 
 
 
-    app.post('/brand', async (req, res) => {
-      const brand = req.body;
-      console.log(brand);
-      const result = await brandNameCollection.insertOne(brand);
+
+
+
+
+    app.post("/samsung", async (req, res) => {
+      const samsung = req.body;
+      const result = await samsungCollection.insertOne(samsung);
+      res.send(result);
+    });
+
+    app.get('/samsung', async (req, res) => {
+      const cursor = samsungCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    app.post("/asus", async (req, res) => {
+      const asus = req.body;
+      const result = await asusCollection.insertOne(asus);
+      res.send(result);
+    });
+
+    app.get('/asus', async (req, res) => {
+      const cursor = asusCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    app.post("/xiaomi", async (req, res) => {
+      const xiaomi = req.body;
+      const result = await xiaomiCollection.insertOne(xiaomi);
+      res.send(result);
+    });
+
+    app.get('/xiaomi', async (req, res) => {
+      const cursor = xiaomiCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    app.post("/sony", async (req, res) => {
+      const sony = req.body;
+      const result = await sonyCollection.insertOne(sony);
       res.send(result);
     });
 
 
+    app.get('/sony', async (req, res) => {
+      const cursor = sonyCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+  });
+
+
+
+
+
+
+
+
+
+
+    app.post("/realme", async (req, res) => {
+      const realme = req.body;
+      const result = await realmeCollection.insertOne(realme);
+      res.send(result);
+    });
+
+    app.get('/realme', async (req, res) => {
+      const cursor = realmeCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+  });
 
 
 
@@ -66,20 +192,26 @@ async function run() {
 
 
 
+
+    app.get("/brand", async (req, res) => {
+      const cursor = brandNameCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
   }
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("brand shopping server is running");
+});
 
-
- app.get('/',(req,res) => {
-    res.send('brand shopping server is running')
- })
-
- app.listen(port, ()=> {
-    console.log(`BrandShopping Server is running on port: ${port}`)
- })
+app.listen(port, () => {
+  console.log(`BrandShopping Server is running on port: ${port}`);
+});
